e1c346de0ca2610ed0a64a4a18d83e155bbb7f96
Paveluts42 <55815780+Paveluts42@users.noreply.github.com>
2019-12-22 12:44:46 +0300

    make loader connect firebase make back

--- src/containers/quizCreator/QuizCreator.js
+++ src/containers/quizCreator/QuizCreator.js
@@ -1,9 +1,11 @@
 import React,{Component} from "react";
 import classes from "./QuizCreator.module.css"
 import Button from "../../components/ui/button/Button";
-import {createControl} from "../../form/formFramework";
+import {createControl,validate,validateForm} from "../../form/formFramework";
 import Input from "../../components/ui/input/Input";
 import Aux from "../../hoc/aux/Aux";
+import Select from "../../components/ui/select/Select";
+import axios from "axios"
 function createOptionControl(number) {
     return createControl({
             label: `Вариант ${number}` ,
@@ -32,19 +34,62 @@ function createFormControls() {
 export default class QuizCreator extends Component{
     state={
         quiz:[],
+        isFormValid: false,
+        rightAnswerId:1,
         formControls: createFormControls()
     }
     submitHandler=event=>{
         event.preventDefault()
     }
-    addQuestionHandler=()=>{
-
+    addQuestionHandler=(event)=>{
+        event.preventDefault()
+const quiz=this.state.quiz.concat()
+        const index =quiz.length+1
+        const {question,option1,option2,option3,option4}=this.state.formControls
+        const questionItem={
+            question:question.value,
+            id:index,
+            rightAnswerId: this.state.rightAnswerId,
+            answers:[
+                {text:option1.value,id:option1.id},
+                {text:option2.value,id:option2.id},
+                {text:option3.value,id:option3.id},
+                {text:option4.value,id:option4.id},
+            ]
+        }
+        quiz.push(questionItem)
+        this.setState({
+            quiz,
+            isFormValid: false,
+            rightAnswerId:1,
+            formControls: createFormControls()
+        })
     }
-    createQuizHandler=()=>{
-
+    createQuizHandler=async event=>{
+        event.preventDefault()
+        try {
+        await axios.post("https://react-quiz-a2653.firebaseio.com/quizes.json",this.state.quiz)
+            this.setState({
+                quiz:[],
+                isFormValid: false,
+                rightAnswerId:1,
+                formControls: createFormControls()
+            })
+        }catch (e) {
+            console.log(e)
+        }
     }
     changeHandler=(value,controlName)=>{
-
+        const formControls={...this.state.formControls}
+        const control={...formControls[controlName]}
+        control.touched=true
+        control.value=value
+        control.valid=validate(control.value,control.validation )
+        formControls[controlName]=control
+this.setState({
+    formControls,
+    isFormValid:validateForm(formControls)
+})
     }
     renderControls=()=>{
         return Object.keys(this.state.formControls).map((controlName,index)=>{
@@ -67,7 +112,24 @@ return(<Aux
 )
         })
     }
+    selectChangeHandler=event=>{
+        this.setState({
+            rightAnswerId:+event.target.value
+        }) }
+
+
     render() {
+        const select= <Select
+        label={"Выберите правильный ответ "}
+        value={this.state.rightAnswerId}
+        onChange={this.selectChangeHandler}
+        option={[
+            {text:1,value:1},
+            {text:2,value:2},
+            {text:3,value:3},
+            {text:4,value:4},
+        ]}
+        />
         return(
             <div className={classes.QuizCreator}>
                 <div>
@@ -75,14 +137,17 @@ return(<Aux
                     <form onSubmit={this.submitHandler}>
                         {this.renderControls()}
 
+                        {select}
 
-                        <select ></select>
                         <Button
                             type="primary"
                             onClick={this.addQuestionHandler}
+                            disabled={!this.state.isFormValid}
                         >Добавить вопрос </Button>
                         <Button
                             type="success"
+                            disabled={!this.state.quiz.length===0}
+
                             onClick={this.createQuizHandler}
                         > Создать тест  </Button>
 
--- src/containers/quizList/QuizList.js
+++ src/containers/quizList/QuizList.js
@@ -1,29 +1,60 @@
 import React,{Component} from "react"
 import classes from "./QuizList.module.css"
 import {NavLink} from "react-router-dom"
+import axios from "axios"
+import Loader from "../../components/ui/loader/Loader";
 export default class QuizList extends Component{
+    state={
+        quizes:[],
+        loading:true,
+    }
     renderQuizes(){
-        return [1,2,3].map((quiz,index)=>{
+        return this.state.quizes.map((quiz)=>{
             return(
                 <li
-                    key={index}
+                    key={quiz.id}
                 >
-<NavLink to={"/quiz/"+quiz}>
-    Тест{quiz}
+<NavLink to={"/quiz/"+quiz.id}>
+    {quiz.name}
 </NavLink>
                 </li>
             )
 
         })
     }
+
+  async  componentDidMount() {
+        try {
+            const response=await axios.get("https://react-quiz-a2653.firebaseio.com/quizes.json")
+            const quizes=[]
+       Object.keys(response.data).forEach((key,index)=>{
+           quizes.push({
+                id:key,
+               name:`test #${index+1}`}
+)
+       })
+            this.setState({
+                quizes,loading:false
+            })
+        }catch (e) {
+            console.log(e)
+        }
+    }
+
     render() {
         return(
             <div className={classes.OuizList}>
 <div>
 <h1>Список тестов</h1>
-    <ul>
-        {this.renderQuizes()}
-    </ul>
+    {this.state.loading
+    ?<Loader/>
+    :
+        <ul>
+            {this.renderQuizes()}
+        </ul>
+    }
+
+
 </div>
             </div>
         )
